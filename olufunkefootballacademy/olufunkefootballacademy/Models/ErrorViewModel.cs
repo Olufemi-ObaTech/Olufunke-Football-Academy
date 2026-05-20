6f82a3ffd7b7dfe69ceb16631460fff74060c5e0
Olufemi-ObaTech <olufemieolugbodi@icloud.com>
2026-05-20 19:33:57 -0400

    Latest updates for Olufunke Football Academy

---
+++ olufunkefootballacademy/olufunkefootballacademy/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace olufunkefootballacademy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
